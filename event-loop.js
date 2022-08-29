324fb39fca171ce271a691a8cf936c4669e97718
Daphne <daphneruth304@gmail.com>
2022-08-29 03:42:23 +0300

    top level code

--- event-loop.js
+++ event-loop.js
@@ -1,2 +1,8 @@
 const fs = require("fs");
 setTimeout(() => console.log("timer 1 finished"), 0);
+setImmediate(() => console.log("timer 1 finished"));
+
+fs.readFile("test-file.txt", "utf8", () => {
+  console.log("I/O finished");
+});
+console.log("top level code finished");
