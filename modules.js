43a6ffbe632816af388c9a0fc0b60923d7a58263
Daphne <daphneruth304@gmail.com>
2022-08-29 12:14:43 +0300

    module.exports

--- modules.js
+++ modules.js
@@ -1,15 +1,6 @@
 // console.log(arguments);
 // console.log(require("module").wrapper);
-class Calculator {
-  add(a, b) {
-    return a + b;
-  }
 
-  multiply(a, b) {
-    return a * b;
-  }
-  divide(a, b) {
-    return a / b;
-  }
-}
-module.exports = Calculator;
+const C = require("./test-module-1");
+const calc1 = new C();
+console.log(calc1.add(2, 5));
