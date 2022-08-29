c2a1aaf5f47f87a363d04fbeaf37c18c6407390b
Daphne <daphneruth304@gmail.com>
2022-08-29 12:20:21 +0300

    updated

--- modules.js
+++ modules.js
@@ -5,3 +5,8 @@
 const C = require("./test-module-1");
 const calc1 = new C();
 console.log(calc1.add(2, 5));
+
+//exports
+
+const { add, multiply } = require("./test-module-2");
+console.log(multiply(2, 5));
