cbf93c1bc3ad5068b44883721daa0b3da46bd012
Daphne <daphneruth304@gmail.com>
2022-08-29 12:05:28 +0300

    exporting single variable

--- modules.js
+++ modules.js
@@ -1,2 +1,15 @@
-console.log(arguments);
-console.log(require("module").wrapper);
+// console.log(arguments);
+// console.log(require("module").wrapper);
+class Calculator {
+  add(a, b) {
+    return a + b;
+  }
+
+  multiply(a, b) {
+    return a * b;
+  }
+  divide(a, b) {
+    return a / b;
+  }
+}
+module.exports = Calculator;
