d2c0b17e6608535fc11f1f8d0bf91a64725ac898
Daphne <daphneruth304@gmail.com>
2022-08-29 03:55:49 +0300

    process.nextTick queue

--- event-loop.js
+++ event-loop.js
@@ -1,11 +1,15 @@
 const fs = require("fs");
+
 setTimeout(() => console.log("timer 1 finished"), 0);
 setImmediate(() => console.log("immediate 1 finished"));
 
 fs.readFile("test-file.txt", "utf8", () => {
   console.log("I/O finished");
+
   setTimeout(() => console.log("timer 2 finished"), 0);
   setImmediate(() => console.log("immediate 2 finished"));
-  setTimeout(() => console.log("timer 2 finished"), 3000);
+  setTimeout(() => console.log("timer 3 finished"), 3000);
+
+  process.nextTick(() => console.log("process.nextTick finished"));
 });
 console.log("top level code finished");
