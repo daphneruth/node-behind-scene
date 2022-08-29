4f7f378a52a4be0880e33615079f31d77d8ecff9
Daphne <daphneruth304@gmail.com>
2022-08-29 11:00:20 +0300

    error event handling

--- streams.js
+++ streams.js
@@ -15,6 +15,11 @@ server.on("request", (req, res) => {
   readable.on("end", () => {
     res.end();
   });
+  readable.on("error", (err) => {
+    console.log(err);
+    res.statusCode = 500;
+    res.end("File not found!");
+  });
 });
 server.listen(8000, "127.0.0.1", () => {
   console.log("listening...");
