f08c18cca0ac85c136224e285612e20eb1d94839
Daphne <daphneruth304@gmail.com>
2022-08-29 10:47:57 +0300

    creating a stream

--- streams.js
+++ streams.js
@@ -2,10 +2,13 @@ const fs = require("fs");
 const server = require("http").createServer();
 
 server.on("request", (req, res) => {
-  fs.readFile("test-file.txt", (err, data) => {
-    if (err) console.log(err);
-    res.end(data);
-  });
+  //   fs.readFile("test-file.txt", (err, data) => {
+  //     if (err) console.log(err);
+  //     res.end(data);
+  //   });
+
+  ///Using Stream
+  const readable = fs.createReadStream("test-file.txt");
 });
 server.listen(8000, "127.0.0.1", () => {
   console.log("listening...");
