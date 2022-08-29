3c8f8ee2a78ca2bd16215f0de52178e9ab88346b
Daphne <daphneruth304@gmail.com>
2022-08-29 08:56:48 +0300

    EventEmiters updated

--- events.js
+++ events.js
@@ -1,3 +1,16 @@
 const EventEmiter = require("events");
 
 const myEmitter = new EventEmitter();
+
+myEmitter.on("newsale", () => {
+  console.log("Newsale received");
+});
+myEmitter.on("newsale", () => {
+  console.log("consumer name: Daphney");
+});
+
+myEmitter.on("newsale", (stock) => {
+  console.log(`there are new ${stock} items left on stock`);
+});
+
+myEmitter.emit("newsale", 100);
